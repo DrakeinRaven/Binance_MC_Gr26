3fc5ff6259b76bb7acc6e3ceb2a314902261ff63
Andre <haaikraai@gmail.com>
2021-03-24 21:16:58 +0200

    Changed the solidity code file to a .sol file and added the bare basics

---
+++ Solidity_Code.sol
@@ -0,0 +1,8 @@
+// Solidity code for project can be documented below for all in group to test on https://bscide.com/
+// All code can be copied, this is comments now :)
+
+pragma solidity ^0.8.0;
+
+contract {
+	
+}
