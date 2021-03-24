1f1e223cfce2eaf22afd0c5fa148f0f9dbdad67d
Andre <haaikraai@gmail.com>
2021-03-24 21:11:16 +0200

    Changed the solidity code file to a .sol file and added the bare framework

---
+++ Solidity Code.sol
@@ -0,0 +1,8 @@
+// Solidity code for project can be documented below for all in group to test on https://bscide.com/
+// Please paste code below this line. 
+
+pragma solidity ^0.8.0;
+
+contract {
+	
+}
